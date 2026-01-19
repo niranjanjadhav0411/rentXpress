fce2a5a4e299a0c1852888b3e196658d402d5f2c
niranjanjadhav0411 <niranjanjadhav0411@gmail.com>
2026-01-19 23:32:18 +0530

    first commit

--- auth-service/src/main/java/com/carrental/auth_service/config/CorsConfig.java
+++ auth-service/src/main/java/com/carrental/auth_service/config/CorsConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 }
--- auth-service/src/main/java/com/carrental/auth_service/dto/BookingRequest.java
+++ auth-service/src/main/java/com/carrental/auth_service/dto/BookingRequest.java
@@ -2,9 +2,11 @@ package com.carrental.auth_service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class BookingRequest {
 
     @NotNull
